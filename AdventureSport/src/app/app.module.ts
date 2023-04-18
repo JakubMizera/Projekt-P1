8dff10c71f0ac06bbd03e04c010b594106e19606
BartoszLukaszek <barti120@onet.pl>
2023-04-18 20:50:46 +0200

    reszta

---
+++ AdventureSport/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BackgroundImageComponent } from './background-image/background-image.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BackgroundImageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

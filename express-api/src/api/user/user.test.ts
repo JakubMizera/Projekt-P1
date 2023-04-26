3ea1ad7ac4fddad106011287df3a8ad4050a586b
Jakub Mizera <66956596+JakubMizera@users.noreply.github.com>
2023-04-26 11:17:55 +0200

    Express api (#8)
    * Express, MongoDb, Zod, Jest with TS template
    
    * basic user model, user route
    
    * delete some unneed code
    
    * simple user test
    
    * connection to mongoDB & updated test, model, route
    
    * moved username and password to .env file

---
+++ express-api/src/api/user/user.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest';
+
+import app from '../../app';
+import { Users } from './user.model';
+
+//import { response } from 'express';
+
+beforeAll(async ()=> {
+    try {
+        await Users.drop();
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+describe('GET /api/v1', () => {
+    it('responds with an array of users', async () => {
+        request(app)
+            .get('/api/v1/user')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toHaveProperty('length');
+                expect(response.body.length).toBe(0);
+            });
+    });
+});

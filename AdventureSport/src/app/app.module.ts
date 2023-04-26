07b8e5e804a08cbee743644ced3e6f028dd2c69d
BartoszLukaszek <barti120@onet.pl>
2023-04-26 22:34:34 +0200

    Add new components

--- AdventureSport/src/app/app.module.ts
+++ AdventureSport/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BackgroundImageComponent } from './background-image/background-image.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BackgroundImageComponent,
+    SearchBarComponent,
+
   ],
   imports: [
     BrowserModule,

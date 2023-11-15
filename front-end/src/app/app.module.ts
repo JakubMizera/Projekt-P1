af95d41223ec1356ccd42b03621a0f346e189497
JakubMizera <kubamiz53@gmail.com>
2023-11-15 12:21:22 +0100

    removed component from app.module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { AboutUsComponent } from './modules/about/about-us/about-us.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutUsComponent,
   ],
   imports: [
     BrowserModule,

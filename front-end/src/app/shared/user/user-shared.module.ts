5965dc20de0b5d1cac9ca12ea1d780c29a7e530f
JakubMizera <kubamiz53@gmail.com>
2023-11-15 17:22:55 +0100

    user custom container

--- front-end/src/app/shared/user/user-shared.module.ts
+++ front-end/src/app/shared/user/user-shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
+import { UserCustomContainerComponent } from './user-custom-container/user-custom-container.component';
 
 @NgModule({
     declarations: [
-        CustomHeaderComponent
+        CustomHeaderComponent,
+        UserCustomContainerComponent
     ],
     imports: [
         CommonModule,
     ],
     exports: [
-        CustomHeaderComponent
+        CustomHeaderComponent,
+        UserCustomContainerComponent
     ]
 })
 export class UserSharedModule { }

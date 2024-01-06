4285c6c329cf034b0b74bb40ac7e4e2926373ce8
JakubMizera <kubamiz53@gmail.com>
2024-01-06 20:25:04 +0100

    products routing module fix

--- front-end/src/app/modules/products/products.module.ts
+++ front-end/src/app/modules/products/products.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProductsViewComponent } from './products-view/products-view.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ProductsViewComponent } from './products-view/products-view.component';
     ReactiveFormsModule,
     MaterialModule,
     SharedModule,
+    ProductsRoutingModule,
   ],
   exports: [],
 })

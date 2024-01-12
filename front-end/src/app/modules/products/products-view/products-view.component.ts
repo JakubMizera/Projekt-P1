a4f59879390ee564229ff60f7b1f7532b1e7b821
JakubMizera <kubamiz53@gmail.com>
2024-01-12 13:05:26 +0100

    Products - implemented courses api

--- front-end/src/app/modules/products/products-view/products-view.component.ts
+++ front-end/src/app/modules/products/products-view/products-view.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/shared/interfaces/course.model';
+import { ApiCoursesService } from 'src/app/shared/user/api-courses.service';
 
 @Component({
   selector: 'app-products-view',
   templateUrl: './products-view.component.html',
   styleUrls: ['./products-view.component.scss']
 })
-export class ProductsViewComponent {
+export class ProductsViewComponent implements OnInit {
+  courses: Course[] = [];
+
+  constructor(
+    private apiCoursesService: ApiCoursesService,
+  ) { }
+
+  ngOnInit(): void {
+    this.apiCoursesService.courses$.subscribe(courses => {
+      this.courses = courses;
+    })
+  }
 
 }

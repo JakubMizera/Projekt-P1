be2eb7ffeb69fc26f32bd2987bfa2bde642c8acc
JakubMizera <kubamiz53@gmail.com>
2024-01-22 18:50:56 +0100

    Implemented courses markers on main map

--- front-end/src/app/modules/main/map/map.component.ts
+++ front-end/src/app/modules/main/map/map.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Course } from 'src/app/shared/interfaces/course.model';
+import { ApiCoursesService } from 'src/app/shared/user/api-courses.service';
 import { CourseBaseComponent } from 'src/app/shared/user/course-base-component/course-base.component';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent extends CourseBaseComponent implements OnInit {
-  constructor(snackBar: MatSnackBar){
+export class MapComponent extends CourseBaseComponent implements OnInit, AfterViewInit {
+  courses!: Course[];
+  override allowMapClickToUpdateLocation = false;
+
+  constructor(
+    private apiCoursesService: ApiCoursesService,
+    snackBar: MatSnackBar
+  ) {
     super(snackBar)
   }
 
   ngOnInit(): void {
     this.initMap();
+    this.apiCoursesService.courses$.subscribe(courses => {
+      this.courses = courses;
+      console.log(courses)
+    })
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showCoursesOnMap(this.courses);
+    }, 100);
+  }
+
+  private showCoursesOnMap(courses: Course[]): void {
+    courses.forEach(course => {
+      if (course.latitude && course.longitude) {
+        const marker = L.marker([course.latitude, course.longitude]).addTo(this.map);
+        marker.bindPopup(`Kurs: ${course.title}`);
+      }
+    })
   }
 
 }

588181b7bdabebf4bcc6eb89c9cf891c12c52e99
JakubMizera <kubamiz53@gmail.com>
2024-01-25 15:18:45 +0100

    Implemented contact with support for logged in user

--- front-end/src/app/modules/user/user-contact/user-contact.component.ts
+++ front-end/src/app/modules/user/user-contact/user-contact.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { ContactService } from 'src/app/shared/contact/contact.service';
+import { UserService } from 'src/app/shared/user/user.service';
 
 @Component({
   selector: 'app-user-contact',
   templateUrl: './user-contact.component.html',
   styleUrls: ['./user-contact.component.scss']
 })
-export class UserContactComponent {
+export class UserContactComponent implements OnInit {
+  contactForm!: FormGroup;
+  userName!: string;
+  userEmail!: string;
+
+  constructor(
+    private contactService: ContactService,
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) { }
+
+
+  ngOnInit(): void {
+    // Subcribe for user name and email
+    this.userService.currentUser$.subscribe(user => {
+      if (user) {
+        this.userName = user.displayName;
+        this.userEmail = user.email;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    })
+
+    this.contactForm = this.formBuilder.group({
+      name: new FormControl(this.userName),
+      email: new FormControl(this.userEmail),
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.contactService.addContact(this.contactForm.value).subscribe({
+        next: () => {
+          this.openSnackBar('Wiadomość została wysłana', 'Zamknij')
+          this.contactForm.reset();
+          // Reset form validation errors
+          Object.keys(this.contactForm.controls).forEach(key => {
+            this.contactForm.get(key)?.setErrors(null);
+          })
+        },
+        error: (error) => console.error(error),
+      });
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, { duration: 3000 });
+  }
 
 }

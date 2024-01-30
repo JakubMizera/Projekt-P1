befaff3194e36e4bd3fdeaa63fd9b9dd54b38801
BartoszLukaszek <barti120@onet.pl>
2024-01-30 17:36:17 +0100

    Validation changes

--- back-end/src/middleware/validation.ts
+++ back-end/src/middleware/validation.ts
@@ -7,7 +7,9 @@ import { CourseCategory } from '../interfaces/CourseCategory';
 // Walidacja użytkownika
 exports.validateUser = [
   check('firstName').not().isEmpty().withMessage('Imię jest wymagane'),
-  check('lastName').not().isEmpty().withMessage('Nazwisko jest wymagane'),
+  check('lastName').not().isEmpty().withMessage('Nazwisko jest wymagane')
+  .matches(/^[a-zA-Z0-9ąęłńóśżźĄĘŁŃÓŚŻŹ\s]+$/)
+
 ];
 
 // Walidacja kursu
@@ -16,13 +18,15 @@ exports.validateCourse = [
   check('title').not().isEmpty().withMessage('Tytuł jest wymagany')
   .isLength({ max: 100 })
   .withMessage('Tytuł kursu może zawierać maksymalnie 100 znaków.')
-  .matches(/^[a-zA-Z0-9\s]+$/)
+  .matches(/^[a-zA-Z0-9ąęłńóśżźĄĘŁŃÓŚŻŹ\s]+$/)
   .withMessage('Tytuł kursu może zawierać tylko litery, cyfry i spacje.')
   ,
 
   //Opis kursu
   check('description').not().isEmpty().withMessage('Opis jest wymagany')
-  .isLength({ max: 1000 }),
+  .isLength({ max: 1000 })
+  .matches(/^[a-zA-Z0-9ąęłńóśżźĄĘŁŃÓŚŻŹ\s]+$/),
+
 
   //Cena kursu
   check('price').not().isEmpty().withMessage('Cena jest wymagana')
@@ -45,7 +49,8 @@ export const validateContact = [
     .trim()
     .not().isEmpty().withMessage('Pole imię i nazwisko jest wymagane.')
     .isLength({ min: 2, max: 50 }).withMessage('Imię i nazwisko musi zawierać od 2 do 50 znaków.')
-    .matches(/^[a-zA-Z\s]*$/).withMessage('Imię i nazwisko może zawierać tylko litery i spacje.'),
+    .matches(/^[a-zA-Z0-9ąęłńóśżźĄĘŁŃÓŚŻŹ\s]+$/)
+    .withMessage('Imię i nazwisko może zawierać tylko litery i spacje.'),
 
   // Walidacja e-maila
   check('email')
@@ -58,6 +63,8 @@ export const validateContact = [
     .trim()
     .not().isEmpty().withMessage('Pole wiadomości jest wymagane.')
     .isLength({ min: 2, max: 1000 }).withMessage('Wiadomość musi zawierać od 10 do 1000 znaków.')
+    .matches(/^[a-zA-Z0-9ąęłńóśżźĄĘŁŃÓŚŻŹ\s]+$/)
+
 ];
 
 export const validate = (req: Request, res: Response, next: NextFunction) => {
